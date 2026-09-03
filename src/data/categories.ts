5f77ed8a552ec271aa095c03bc6ea073e2e1d512
fumetothemoon <fumetothemoon@gmail.com>
2026-09-03 22:33:18 +0800

    add all shopee product and update text

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,14 +1,14 @@
 // Allowed category names. Add a new member here before using it in products.ts.
 export enum CategoryName {
-  MustHave = "超好用",
-  Usable = "堪用",
+  MustHave = "瘋狂安麗",
+  FrequentlyUsed = "一再回購",
   LookingGood = "外貿協會",
 }
 
 export enum CategoryDescription {
-  MustHave = "用過一次就回不去...",
-  Usable = "好用可以用 會回購 (除非被我遇到更好的選擇",
-  LookingGood = "顏值扛住了實力",
+  MustHave = "用過一次就綁死(〃∀〃)",
+  FrequentlyUsed = "好用會再買 (不排除喜新厭舊",
+  LookingGood = "顏值太暈(*´∀`)~♥實力夠用就好",
 }
 
 export const categoryNames = Object.values(CategoryName);
